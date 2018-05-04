25611f861f3f54535561a50ddc1e743bfa247d14
Miriam Reichlmaier <miriam.reichlmaier@googlemail.com>
2018-05-04 13:04:23 +0200

    abstrakte Klasse body, einzelne Körper extenden die jetzt

---
+++ src/Cylinder.java
@@ -0,0 +1,29 @@
+
+public final class Cylinder extends Body{
+
+	@Override
+	public double getSurface() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
---
+++ src/Sphere.java
@@ -0,0 +1,28 @@
+
+public final class Sphere extends Body{
+
+	@Override
+	public double getSurface() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getVolume() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
