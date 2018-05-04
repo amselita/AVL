b929c7b2ebb78699881c31c5a4b9b30dfbe90157
Miriam Reichlmaier <miriam.reichlmaier@googlemail.com>
2018-05-04 13:08:23 +0200

    Body fertig?

--- src/Body.java
+++ src/Body.java
@@ -2,9 +2,27 @@
 
 abstract class Body{
 	
+	/**
+	 * calculates the surface of an body
+	 * @return double value the surface
+	 */
 	public abstract double getSurface();
+	/**
+	 * calculates the volume of an body
+	 * @return double value volume
+	 */
 	public abstract double getVolume();
+	/**
+	 * Prints the string representation of a body to the console
+	 * 
+	 */
 	public abstract String toString();
+	
+	/**
+	 * compares an object to ???
+	 * @param: Object o which shall be compared
+	 */
+	//TODO
 	public abstract boolean equals(Object o);
 	
 	
