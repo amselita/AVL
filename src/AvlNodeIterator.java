9b741a372152dea6815e17fa9be9652db4635304
Miriam Reichlmaier <miriam.reichlmaier@googlemail.com>
2018-05-13 01:43:42 +0200

    Removed next() comments

--- src/AvlNodeIterator.java
+++ src/AvlNodeIterator.java
@@ -29,7 +29,7 @@ public class AvlNodeIterator<K,  V> implements Iterator<AvlNode<K, V>> {
 		
 		if (current.getRight()!=null){
 			next=current.getRight().findMin();
-		} 
+		}
 		else { 
 			AvlNode<K, V> pointer=current;
 			
@@ -45,32 +45,5 @@ public class AvlNodeIterator<K,  V> implements Iterator<AvlNode<K, V>> {
 			}
 		}
 		return current;
-
-
-//		//if (last != null) {// schauen ob ich beim min/start bin
-//			last = next; // last wird dem dem gespeicherten Wert vom vorherigen
-//							// Current belegt
-//			// neues current
-//			// wenn ich im lnken Knpten war ist kmein neues current der parent
-//			if (current.getParent().getLeft() == last) {
-//				next = next.getParent();
-//
-//				// wenn ich einen rechten Subtree habe ist das neue current das
-//				// Minimum davon
-//			} else if (current.getParent().getRight() != null) {
-//				next = current.getParent().getRight().findMin();
-//
-//				// und sonst wenn ich links schon hab und mein last der parnent
-//				// ist und es keinen rechten Subtree gibt
-//
-//			} else if (current.getRight() == last && current.getParent() != null) {
-//				next = current.getParent();
-//			} else {
-//				next = null;
-//			}
-////		} else {
-////			last = next;
-////		}
-
 	}
 }
