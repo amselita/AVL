8fa17cba24338569754e7bb25ad3549df8e7abba
Miriam Reichlmaier <miriam.reichlmaier@googlemail.com>
2018-05-13 01:39:39 +0200

    NEXT FUNKTIONIERT UND IST FERTIG und mein Testding

--- src/AvlNodeIterator.java
+++ src/AvlNodeIterator.java
@@ -1,19 +1,76 @@
 import java.util.Iterator;
 
-//Iterator über Werte, nur V
 
 public class AvlNodeIterator<K,  V> implements Iterator<AvlNode<K, V>> {
-	AvlNode<K, V> current=root;
+	AvlNode<K, V> next;
+	AvlNode<K, V> last;
+
 	
+	public AvlNodeIterator(AvlNode<K, V> min) {
+		next=min;
+		last=null;
+	}
 	
-		@Override
-		public boolean hasNext(AvlNode<K, V> node){
-			if (current.hasLeft()==true|| current.hasRight()==true)
-			return true;
-		}
-		@Override
-			public AvlNode<K, V> next(){
+	@Override
+	public boolean hasNext(){
+		//return (current.getLeft()!=null|| current.getRight()!=null);
+		return next != null;
+	}
+
+	/**
+	 * das in next gespeicherte next wird ausgegeben
+	 * generates das next für den nächsten next aufruf in der vairale next ab
+	 * für den ersten aufruf ist das min schon als next gesetzt
+	 * 
+	 */
+	@Override
+	public AvlNode<K, V> next() {
+		AvlNode<K, V> current = next;
+		
+		if (current.getRight()!=null){
+			next=current.getRight().findMin();
+		} 
+		else { 
+			AvlNode<K, V> pointer=current;
 			
-			return 
+			if ( pointer.getParent().getLeft()==pointer){
+				next=pointer.getParent();
+			} 
+			else {
+				while (pointer.getParent()!=null && pointer.getParent().getRight()==pointer){
+					pointer=pointer.getParent();
+				}
+				
+				next=pointer.getParent();
+			}
 		}
+		return current;
+
+
+//		//if (last != null) {// schauen ob ich beim min/start bin
+//			last = next; // last wird dem dem gespeicherten Wert vom vorherigen
+//							// Current belegt
+//			// neues current
+//			// wenn ich im lnken Knpten war ist kmein neues current der parent
+//			if (current.getParent().getLeft() == last) {
+//				next = next.getParent();
+//
+//				// wenn ich einen rechten Subtree habe ist das neue current das
+//				// Minimum davon
+//			} else if (current.getParent().getRight() != null) {
+//				next = current.getParent().getRight().findMin();
+//
+//				// und sonst wenn ich links schon hab und mein last der parnent
+//				// ist und es keinen rechten Subtree gibt
+//
+//			} else if (current.getRight() == last && current.getParent() != null) {
+//				next = current.getParent();
+//			} else {
+//				next = null;
+//			}
+////		} else {
+////			last = next;
+////		}
+
+	}
 }
---
+++ src/AvlTestMap.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+
+public class AvlTestMap extends AvlTreeMap<String, Integer> {
+	public AvlTestMap (){
+		super(new MyComparator());
+		
+		root = new AvlNode<String, Integer>("Grashalm", 5, null);
+		AvlNode<String, Integer> left = new AvlNode<String, Integer>("stein", 4, root);
+		root.setLeft(left);
+		AvlNode<String, Integer> right = new AvlNode<String, Integer>("blume", 6, root);
+		root.setRight(right);
+		
+	}
+
+	public static void main(String[] args) {
+		// generate test map
+		AvlTestMap testMap = new AvlTestMap();
+		
+		// print keys
+		Iterator<AvlNode<String, Integer>> iterator = testMap.iterator();
+		while(iterator.hasNext() == true) {
+			System.out.println("Key: " + iterator.next().getKey());
+		}
+	}
+
+}
